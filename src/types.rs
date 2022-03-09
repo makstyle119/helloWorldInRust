e79592ec0cab8516db4e1a2a5abc2caa6802af8b
moizalikhan119 <moizprince786@gmail.com>
2022-03-09 22:20:26 +0500

    types

--- src/types.rs
+++ src/types.rs
@@ -10,4 +10,33 @@ Arrays
 
 // Rust is a statically typed language, which means that it must know the types of all variables at compile time, however, the compiler can usually infer what type we want to use based on the value and how we use it.
 
-pub fn run() {}
+pub fn run() {
+  // Default is "i32"
+  let x = 1;
+
+  // Default is "f64"
+  let y = 2.5;
+
+  // add explicit type
+  let z: i64 = 12321;
+
+  // Find max size
+  println!("Max i32: {}", std::i32::MAX);
+  println!("Max i64: {}", std::i64::MAX);
+
+  // Boolean
+  let is_active = true;
+  let is_default: bool = true;
+
+  // Get Boolean from expression
+  let is_greater: bool = 10 > 12;
+
+  let a1 = 'a';
+
+  let face = '\u{1F600}';
+
+  println!(
+    "{:?}",
+    (x, y, z, is_active, is_default, is_greater, a1, face)
+  );
+}
